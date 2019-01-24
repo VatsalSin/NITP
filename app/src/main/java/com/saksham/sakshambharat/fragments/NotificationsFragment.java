6d5f7c0983634903fa7e82ead856f196b527dc00
amangrobo <amangrobo@gmail.com>
2019-01-24 23:44:49 +0530

    minor changes

--- app/src/main/java/com/saksham/sakshambharat/fragments/NotificationsFragment.java
+++ app/src/main/java/com/saksham/sakshambharat/fragments/NotificationsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,8 +31,11 @@ public class NotificationsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+        toolbar.setTitle("Notifications");
+
         recyclerView = v.findViewById(R.id.recyclerview);
         LinearLayoutManager manager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(manager);
